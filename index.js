53c029a2076d8c262a047cb4cc0a7ecda4c22348
razken-hash <ka_kenniche@esi.dz>
2024-05-20 22:20:04 +0100

    CORS removed from index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const authRouter = require("./routes/auth");
 const cors = require("cors");
 
-const PORT = 2020;
+const PORT = 3001;
 const DB = "mongodb+srv://razken-hash:a8Tx6Oj4SojxZ2ca@tera-cluster.qoyzisk.mongodb.net/?retryWrites=true&w=majority&appName=tera-cluster";
 
 mongoose.connect(DB).then(() => {
@@ -14,8 +14,8 @@ mongoose.connect(DB).then(() => {
 
 const app = express();
 
-app.use(cors);
 app.use(express.json());
+// app.use(cors);
 app.use(authRouter);
 
 app.listen(PORT, "0.0.0.0", () => {
