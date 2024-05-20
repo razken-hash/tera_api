dc58b370209697be011da555bb01068c8c5b81c2
razken-hash <ka_kenniche@esi.dz>
2024-05-20 19:05:48 +0100

    Connection To MongoDB Established

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const jwt = require("jsonwebtoken");
 
 const PORT = 2020;
 
+const DB = "mongodb+srv://razken-hash:a8Tx6Oj4SojxZ2ca@tera-cluster.qoyzisk.mongodb.net/?retryWrites=true&w=majority&appName=tera-cluster";
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection To MongoDB Established!");
+})
+
 const app = express();
 
 app.listen(PORT, "0.0.0.0", () => {
